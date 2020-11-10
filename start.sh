d275db453f37dd729f5aec3fbfd123f8fa9e5683
ducpv <phanvnduc@gmail.com>
2020-11-10 11:14:16 +0700

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+echo '[+] Install docker'
+./installDocker.sh
 echo '[!] Edit Shodan API first'
 vi Dockerfile
-echo '[!] Login docker'
+echo '[+] Login docker'
 docker login
-echo '[!] Docker build'
+echo '[+] Docker build'
 docker build -t scoutv2 .
+echo '[!] Remove archive'
+docker rmi justkiddingfd/scout
